c226dd7291967964cf598dfa3bdfc49a821c674a
Useinov Eugene <evokerr666@gmail.com>
2022-09-06 12:37:05 +0300

    Исправление конфликтов

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,11 +8,8 @@ import { CloseCircleOutlined } from '@ant-design/icons';
 import DataTable from './DataTable';
 const { Header, Content } = Layout;
 
-
 const HEADER_STYLE={display: 'flex', justifyContent:'left', alignItems: 'center'}
 const BUTTON_STYLE={alignItems: 'center'}
-
-
 
 const Navigation = () => {
   const [stateList, setStateList] = useState([]); // Хранилище вкладок и его обновление
@@ -27,7 +24,6 @@ const Navigation = () => {
       setStateFormValues('')
     }
   };
-
   const remove = (key) => { // Функция удаления вкладки по id
     const index = stateList.findIndex(item => item.key === key); // Ищем в хранилище вкладок вкладку по id и присваиваем её index
     Modal.confirm({
@@ -43,84 +39,58 @@ const Navigation = () => {
         }
       }
     })
-    
-    
   };
-
   const handleChange = (e) => { // Функция записи данных в хранилище формы
     const value = e?.target?.value;
     setStateFormValues(value); // Обновляем старое хранилище формы на новое
   }
-
   const showModal = () => {
     setIsModalVisible(true);
   };
-
   const handleOk = () => {
     create();
     setIsModalVisible(false);
   };
-
   const handleCancel = () => {
     setIsModalVisible(false);
   };
-  
   return (
-  <Layout className="layout">
-    
-    <Modal 
-        bodyStyle={{ display: 'flex'}}
-        title="Введите название новой вкладки"
-        cancelText="Отмена"
-        visible={isModalVisible} 
-        onOk={handleOk} 
-        onCancel={handleCancel}>
-          <div style={{display: 'flex'}}>
-            <input placeholder="Название вкладки" style={{width: 365}} value={stateFormValues} onChange={handleChange} />
-            <Button style={{marginLeft:'20px'}} onClick={() => create()}>Добавить</Button>
-          </div>
-      </Modal>
-    <Header style={HEADER_STYLE}>
-      <Button style={BUTTON_STYLE} type="primary" onClick={() => showModal()}>Создать</Button>
-      {/* <Menu
-        style={{}}
-        theme="dark"
-        mode="horizontal"
-        items={stateList.map((item) => {
-          const label = item.label
-          const key = item.id
-            return {
-              key,
-              label,
-            }
-        })}
-      /> */}
-      <div style={{width:'90%', margin: '3em'}}>
-      <Menu theme="dark" mode="horizontal"> {
-        stateList.map((item) => (
-          console.log(stateList),
-          <Menu.Item>
-            {item.label}
-            {/* <Button style={{marginLeft: '10px', width:'20px', height:'20px'}} type="text" danger onClick={() => remove(item.key)}>x</Button> */}
-            <CloseCircleOutlined title='Удалить' style={{color:'red', marginLeft:'.5em', fontSize:'1em'}}onClick={(e) => {
-              e.stopPropagation();
-              remove(item.key)
-            }}/>
-          </Menu.Item>))
-      }
-      </Menu>
-      </div>
-    </Header>
-    <Content
-      style={{
-        padding: '50px 50px',
-      }}
-    >
-      <div className="site-layout-content">
-        <DataTable />
-      </div>
-    </Content>
-  </Layout>
+    <Layout className="layout">
+      <Modal 
+          bodyStyle={{ display: 'flex'}}
+          title="Введите название новой вкладки"
+          cancelText="Отмена"
+          visible={isModalVisible} 
+          onOk={handleOk} 
+          onCancel={handleCancel}>
+            <div style={{display: 'flex'}}>
+              <input placeholder="Название вкладки" style={{width: 365}} value={stateFormValues} onChange={handleChange} />
+              <Button style={{marginLeft:'20px'}} onClick={() => create()}>Добавить</Button>
+            </div>
+        </Modal>
+      <Header style={HEADER_STYLE}>
+        <Button style={BUTTON_STYLE} type="primary" onClick={() => showModal()}>Создать</Button>
+        <div style={{width:'90%', margin: '3em'}}>
+        <Menu theme="dark" mode="horizontal"> {
+          stateList.map((item) => (
+            console.log(stateList),
+            <Menu.Item>
+              {item.label}
+              <CloseCircleOutlined title='Удалить' style={{color:'red', marginLeft:'.5em', fontSize:'1em'}}onClick={(e) => {
+                e.stopPropagation();
+                remove(item.key)
+              }}/>
+            </Menu.Item>))
+        }
+        </Menu>
+        </div>
+      </Header>
+      <Content style={{padding: '50px 50px'}}>
+        <div className="site-layout-content">
+          <DataTable />
+        </div>
+      </Content>
+    </Layout>
 )};
 
 export default Navigation;
