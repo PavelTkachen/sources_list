ed559ad5d0dd74ff47b71f6792c2032180f7e80e
Useinov Eugene <evokerr666@gmail.com>
2023-03-07 13:59:22 +0400

    [I-7] Сделал задание 15

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import Navigation from './pages/Navigation';
-import AuthPage from './pages/AuthPage';
+import React from "react";
+import MainPage from "./pages/MainPage";
+import AuthPage from "./pages/AuthPage";
+import { Route, Routes } from "react-router-dom";
+import { RequireAuth } from "./helpers/requireAuth";
 
 function App() {
   return (
-        <AuthPage />
-      //<Navigation />
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <MainPage />
+            </RequireAuth>
+          }
+        />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </>
   );
-};
+}
 
 export default App;
